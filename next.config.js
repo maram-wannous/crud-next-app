c53ffdcf9217f1588ff076e3b506e8360177e82a
maram_wannous <maram.wannous@gmail.com>
2023-11-11 10:56:47 +0200

    mmm

--- next.config.js
+++ next.config.js
@@ -1,47 +1,46 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-}
+const nextConfig = {}
 
-module.exports = nextConfig;
-    // images: {
-    //   remotePatterns: [
-    //     {
-    //       protocol: 'https',
-    //       hostname: "i0.wp.com",
-    //     },
-    //     {
-    //       protocol: 'https',
-    //       hostname: "www.vec.ca",
-    //     },
-    //     {
-    //       protocol: 'https',
-    //       hostname: "www.training.com.au",
-    //     },
-    //     {
-    //       protocol: 'https',
-    //       hostname: "www.dundryprimary.co.uk",
-    //     },
-    //     {
-    //       protocol: 'https',
-    //       hostname: "img.freepik.com",
-    //     },
-    //     {
-    //       protocol: 'https',
-    //       hostname: "miro.medium.com",
-    //     },
-    //     {
-    //       protocol: 'https',
-    //       hostname: "usa.bootcampcdn.com",
-    //     },
-    //     {
-    //       protocol: 'https',
-    //       hostname: "www.scalevp.com",
-    //     },
-    //     {
-    //       protocol: 'https',
-    //       hostname: "www.rwjbh.org",
-    //     },
-    //   ],
-    // },
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: "i0.wp.com",
+      },
+      {
+        protocol: 'https',
+        hostname: "www.vec.ca",
+      },
+      {
+        protocol: 'https',
+        hostname: "www.training.com.au",
+      },
+      {
+        protocol: 'https',
+        hostname: "www.dundryprimary.co.uk",
+      },
+      {
+        protocol: 'https',
+        hostname: "img.freepik.com",
+      },
+      {
+        protocol: 'https',
+        hostname: "miro.medium.com",
+      },
+      {
+        protocol: 'https',
+        hostname: "usa.bootcampcdn.com",
+      },
+      {
+        protocol: 'https',
+        hostname: "www.scalevp.com",
+      },
+      {
+        protocol: 'https',
+        hostname: "www.rwjbh.org",
+      },
+    ],
+  },
+}
+   
